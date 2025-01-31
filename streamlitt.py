e87198adeac1839b7cb9a6989a2cfe2e11436f12
Supriya-RK <117072689+Supriya-RK@users.noreply.github.com>
2025-01-31 14:09:47 +0530

    Update streamlitt.py

--- streamlitt.py
+++ streamlitt.py
@@ -24,7 +24,7 @@ A sincere individual with strong values working skilled in problem solving, look
 # Navigation
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-
+st.write('### Tata Cosultancy Services")
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
   <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Supriya Pandey</a>
